f32fc8367818e920064d56b1d28a07f1d5484d9f
Rafał Kwiecień <kwiecienraf@gmail.com>
2018-04-06 15:50:06 +0200

    Setter Injection

--- src/main/java/demo_spring/CricketCoach.java
+++ src/main/java/demo_spring/CricketCoach.java
@@ -4,8 +4,38 @@ public class CricketCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    private String emailAdress;
+    private String team;
+
+    public CricketCoach() {
+        System.out.println("Cricket Coach: inside no-arg constructor");
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        System.out.println("Cricket Coach: inside setter method - setEmailAdress");
+        this.emailAdress = emailAdress;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Cricket Coach: inside setter method - setTeam");
+        this.team = team;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("Cricket Coach: inside setter method - setFortuneService");
+        this.fortuneService = fortuneService;
+    }
+
     public String getDailyWorkout() {
-        return "Practice fst bowling for 15 minutes";
+        return "Practice fast bowling for 15 minutes";
     }
 
     public String getDailyFortune() {
---
+++ src/main/java/demo_spring/SetterDemoApp.java
@@ -0,0 +1,25 @@
+package demo_spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+    public static void main(String[] args) {
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("appContext.xml");
+
+        // retrieve bean from spring container
+
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
+
+        // call methods on the bean
+
+        System.out.println(theCoach.getDailyWorkout());
+        System.out.println(theCoach.getDailyFortune());
+
+        System.out.println(theCoach.getEmailAdress());
+        System.out.println(theCoach.getTeam());
+        // close the context
+
+        context.close();
+    }
+}
